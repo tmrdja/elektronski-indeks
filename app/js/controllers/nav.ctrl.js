338dfb7bfe3ae42481b94fd7d7fd14c5875fdb02
tmrdja <t.mrdja@hotmail.com>
2016-10-07 17:47:34 +0200

    Selected tab

--- app/js/controllers/nav.ctrl.js
+++ app/js/controllers/nav.ctrl.js
@@ -1,15 +1,16 @@
-angular.module('app').controller('navCtrl', ['$scope', 'Service', 'AuthService', '$state', function ViewCtrl($scope, Service, AuthService, $state) {
+angular.module('app').controller('navCtrl', ['$scope', 'Service', 'AuthService', '$state', '$rootScope', function ViewCtrl($scope, Service, AuthService, $state, $rootScope) {
     var self = this;
-    this.state = $state;
     console.log('state', $state)
 
     this.getUser = function () {
         return AuthService.user;
     }
 
-    $scope.$watch('state.current.name', function (val) {
-        this.selectedTab = val;
-    });
+    $scope.$watch(function () {
+        return $state.$current.name
+    }, function (newVal, oldVal) {
+        self.selectedTab = newVal;
+    })
 
     this.logout = function () {
         AuthService.logout();
